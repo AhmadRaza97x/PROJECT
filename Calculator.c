41799e794142fc30f138420e6103646571ec2905
Mohammad Ahamad Raza <ahmadraza97x@gmail.com>
2025-02-28 00:28:55 +0530

    Updated Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <stdlib.h>
-
-void calculator() {
-    double num1, num2, result;
-    char operator;
-
-    while (1) {
-        printf("\nEnter an operation (+, -, *, /) or 'q' to quit: ");
-        scanf(" %c", &operator); // Space before %c to ignore whitespace
-
-        if (operator == 'q') {
-            printf("Exiting calculator...\n");
-            break;
-        }
-
-        printf("Enter two numbers: ");
-        scanf("%lf %lf", &num1, &num2);
-
-        switch (operator) {
-            case '+': result = num1 + num2; break;
-            case '-': result = num1 - num2; break;
-            case '*': result = num1 * num2; break;
-            case '/':
-                if (num2 == 0) {
-                    printf("Error! Division by zero is not allowed.\n");
-                    continue;
-                }
-                result = num1 / num2;
-                break;
-            default:
-                printf("Invalid operation! Please enter +, -, *, or /.\n");
-                continue;
-        }
-
-        printf("Result: %.2lf\n", result);
-    }
-}
-
-int main() {
-    calculator();
-    return 0;
-}
-=======
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -91,4 +46,4 @@ int main() {
     calculator();
     return 0;
 }
->>>>>>> 18922d4 (Updated Calculator.c)
+
