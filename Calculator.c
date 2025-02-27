27f938009433ef9d5c057956cbff4aac6e3fa366
Mohammad Ahamad Raza <ahmadraza97x@gmail.com>
2025-02-28 00:23:12 +0530

    Fixed conflicts in Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -41,3 +42,53 @@ int main() {
     calculator();
     return 0;
 }
+=======
+#include <stdio.h>
+#include <stdlib.h>
+
+void calculator() 
+{
+    double num1, num2, result;
+    char operator;
+
+    while (1) 
+    {
+        printf("\nENTER OPERATION (+, -, *, /) or 'q' == EXIT : ");
+        scanf(" %c", &operator);
+
+        if (operator == 'q') 
+        {
+            printf("\nEXIT..\n");
+            break;
+        }
+        printf("\nENTER NO. 1 : ");
+        scanf("%lf", &num1);
+
+        printf("\nENTER NO. 2 : ");
+        scanf("%lf", &num2);
+
+        switch (operator) 
+        {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/':
+                if (num2 == 0) {
+                    printf("\nERROR ! ..DIVISION BY 0 NOT ALLOWED \n");
+                    continue;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                printf("\nOPERATION NOT FOUND\n");
+                continue;
+        }
+        printf("\nRESULT : %.2lf\n\n", result);
+    }
+}
+
+int main() {
+    calculator();
+    return 0;
+}
+>>>>>>> 18922d4 (Updated Calculator.c)
